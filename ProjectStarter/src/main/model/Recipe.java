f4f391baf1f9301e4d08cd3ed27c544d569ead0c
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-12 01:05:40 -0800

    Added fields and class specification for Recipe

--- ProjectStarter/src/main/model/Recipe.java
+++ ProjectStarter/src/main/model/Recipe.java
@@ -1,7 +1,16 @@
 package model;
 
+import java.util.*;
+
+// Represents a recipe having a name, a list of ingredients, a estimated cooking
+// time (in minutes), a cuisine type, and an estimated cost (in dollars)
 public class Recipe {
-    int other;
+
+    private String name; // recipe name
+    private List<Ingredient>ingredients; // the required ingredients
+    private int cookingTime; // the estimated cooking time
+    private String cuisineType; // the cuisine type
+    private double cost; // the estimated cost
 
     public Recipe() {
         other = 4;
