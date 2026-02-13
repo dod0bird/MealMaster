4b0533cb0d4d247e41e68d97d520cf7f24495e0a
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-13 11:25:15 -0800

    added ingredient specification

--- ProjectStarter/src/main/model/Ingredient.java
+++ ProjectStarter/src/main/model/Ingredient.java
@@ -1,5 +1,31 @@
 package model;
 
+// Represents an ingredient having a name, quantity, and unit of measurement
 public class Ingredient {
+    private String ingredientName;     // ingredient name
+    private double ingredientQuantity; // ingredient quantity
+    private String ingredientUnit;      // ingredient unit
+
+    /*
+    * REQUIRES: name has non-zero length, quantity > 0, unit has non-zero length
+    * EFFECTS: create a new ingredient with specified name and quantity
+    */
+    public Ingredient(String name, double quantity, String units) {
+        this.ingredientName = name;
+        this.ingredientQuantity = quantity;
+        this.ingredientUnit = units;
+    }
+
+    public String getIngredientName() {
+        return "";
+    }
+
+    public double getIngredientQuantity() {
+        return 1;
+    }
+
+    public String getIngredientUnit() {
+        return "";
+    }
 
 }
