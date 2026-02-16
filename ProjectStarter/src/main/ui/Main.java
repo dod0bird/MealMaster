d6e3ced5b401fedd35734cbf40a56349f9c56e9b
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-15 18:56:05 -0700

    Provided specification and implementation for Main class

--- ProjectStarter/src/main/ui/Main.java
+++ ProjectStarter/src/main/ui/Main.java
@@ -1,7 +1,9 @@
 package ui;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+
+    // EFFECTS: create a new meal master app
+    public static void main(String[] args) {
+        new MealMasterApp();
     }
 }
