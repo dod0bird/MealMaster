776f7e35bb2c220755ffdf269edf95877c859fb7
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-15 17:13:28 -0700

    Added specification for RecipeBook

--- ProjectStarter/src/main/model/RecipeBook.java
+++ ProjectStarter/src/main/model/RecipeBook.java
@@ -1,5 +1,13 @@
 package model;
 
-public class RecipeBook {
+import java.util.*;
 
+// Represents a user's collection of recipes
+public class RecipeBook {
+    private List<Recipe> recipes; // list of recipes
+
+    // EFFECTS: create a new empty recipe collection
+    public RecipeBook() {
+        recipes = new ArrayList<Recipe>();
+    }
 }
