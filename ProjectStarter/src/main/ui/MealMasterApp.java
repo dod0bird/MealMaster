7e4f0cf9c3bac8c46e64a168b7d59129454fc435
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-15 19:10:25 -0700

    added specification for MealMasterApp class

---
+++ ProjectStarter/src/main/ui/MealMasterApp.java
@@ -0,0 +1,59 @@
+package ui;
+
+import model.*;
+import java.util.*;
+
+public class MealMasterApp {
+    private RecipeBook recipeBook;
+    private Scanner input;
+
+    // EFFECTS: runs the Meal Master application
+    public MealMasterApp() {
+        startNewApp();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input and runs the application
+    private void startNewApp() {
+        boolean keepGoing = true;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void processCommand(command) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes recipes and scanner
+    private void init() {
+        input = new Scanner(System.in);
+    }
+
+    // EFFECTS: displays the menu options to the user
+    private void displayMenu() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a recipe to the collection
+    private void doAddRecipe() {
+
+    }
+
+    // EFFECTS: displays all recipes in the collection
+    private void doViewRecipes() {
+
+    }
+
+    // EFFECTS: search recipes by ingredient, cuisine, cookingtime, or cost
+    private void doSearchRecipes() {
+
+    }
+
+    // EFFECTS: generates a grocery list from all recipes
+    private void doGroceryList() {
+
+    }
+
+}
