9cd29f199220ed2bb839dc4e4712bc68112407e9
Emily Tu <emilyt72@students.cs.ubc.ca>
2026-02-26 17:41:22 -0800

    Added specification for JsonWriter Class

---
+++ ProjectStarter/src/main/persistence/JsonWriter.java
@@ -0,0 +1,34 @@
+package persistence;
+import model.*;
+import org.json.JSONObject;
+
+import java.io.*;
+
+// this code was inspiried by the code in the JsonSerializationDemo project provided.
+
+public class JsonWriter {
+
+    // EFFECTS: creates a writer to write to destination file
+    public JsonWriter(String destination) {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer
+    public void open() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes the JSON representation of recipebook to file
+    public void write() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: saves string to file
+    public void saveToFile() {
+    }
+}
